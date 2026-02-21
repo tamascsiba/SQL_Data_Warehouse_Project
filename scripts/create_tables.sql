b60624252ec4a90eeba6957d970c3f02c832f463
Tamas_Csiba <csiba.toma1@gmail.com>
2026-02-21 15:43:20 +0100

    Update create_tables.sql

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -63,26 +63,26 @@ CREATE TABLE bronze.crm_sales_details(
 	sls_price INT
 );
 
-IF OBJECT_ID('bronze.erp_CUST_AZ12' , 'U') IS NOT NULL
-	DROP TABLE bronze.erp_CUST_AZ12;
-CREATE TABLE bronze.erp_CUST_AZ12(
-	CID NVARCHAR(50),
-	BDATE DATE,
-	GEN NVARCHAR(50),
+IF OBJECT_ID('bronze.erp_cust_az12' , 'U') IS NOT NULL
+	DROP TABLE bronze.erp_cust_az12;
+CREATE TABLE bronze.erp_cust_az12(
+	cid NVARCHAR(50),
+	bdate DATE,
+	gen NVARCHAR(50),
 );
 
-IF OBJECT_ID('bronze.erp_LOC_A101' , 'U') IS NOT NULL
-	DROP TABLE bronze.erp_LOC_A101;
-CREATE TABLE bronze.erp_LOC_A101(
-	CID NVARCHAR(50),
-	CNTRY NVARCHAR(50),
+IF OBJECT_ID('bronze.erp_loc_a101' , 'U') IS NOT NULL
+	DROP TABLE bronze.erp_loc_a101;
+CREATE TABLE bronze.erp_loc_a101(
+	cid NVARCHAR(50),
+	nctry NVARCHAR(50),
 );
 
-IF OBJECT_ID('bronze.erp_PX_CAT_G1V2' , 'U') IS NOT NULL
-	DROP TABLE bronze.erp_PX_CAT_G1V2;
-CREATE TABLE bronze.erp_PX_CAT_G1V2(
-	ID NVARCHAR(50),
-	CAT NVARCHAR(50),
-	SUBCAT NVARCHAR(50),
-	MAINTANCE NVARCHAR(50)
+IF OBJECT_ID('bronze.erp_px_cat_g1v2' , 'U') IS NOT NULL
+	DROP TABLE bronze.erp_px_cat_g1v2;
+CREATE TABLE bronze.erp_px_cat_g1v2(
+	id NVARCHAR(50),
+	cat NVARCHAR(50),
+	subcat NVARCHAR(50),
+	maintance NVARCHAR(50)
 );
